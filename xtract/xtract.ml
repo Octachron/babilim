ef66fdb1d9525564dd93bc4a3ffb475d14c7b08f
octachron <octa@polychoron.fr>
2017-11-29 19:51:23 +0100

    Initial extracter

---
+++ xtract/xtract.ml
@@ -0,0 +1,294 @@
+module Iter = Ast_iterator
+module L = Longident
+
+let debug fmt = Format.eprintf (fmt ^^ "@.")
+open Parsetree
+
+let i18n_key = function
+  | { Location.txt = "i18n"; _ }, _ ->  true
+  | _ -> false
+
+let status = ref false
+
+
+type multiline = string list
+type comments =
+  {
+    translator: multiline;
+    programmer: multiline
+  }
+
+type loc = { file:string; line:int}
+
+type msg =
+  | Singular of { id:multiline; translation:multiline }
+  | Plural of {id:multiline;plural:multiline; translations: (int * multiline) list }
+
+type entry = {
+  comments: comments;
+  location: loc;
+  flags: string list;
+  context: multiline;
+  previous: msg option;
+  msg: msg;
+}
+
+
+type info = { plural: bool; context: string list }
+let info_default = { plural = false; context = [] }
+
+let metadata e =
+  let label r (l,x) = match (l.Location.txt,x) with
+    | L.Lident "plural", _  -> { r with plural = true }
+    | L.Lident "context", { pexp_desc = Pexp_constant Pconst_string (s,_) } ->
+      { r with context = s :: r.context }
+    | _ -> r
+  in
+  let start = function
+    | Some {pexp_desc= Pexp_ident { txt = L.Lident "plural";_}; _ } ->
+      { info_default with plural = true }
+    | _ -> info_default in
+  match e.pexp_desc with
+  | Pexp_record (fields,x) -> List.fold_left label (start x) fields
+  | _ -> info_default
+
+
+let maybe f x = match x with
+  | Some x -> f x
+  | None -> ()
+
+let bind f x =  match x with
+  | Some x -> f x
+  | None -> None
+
+
+let (>>) x f = maybe f x
+let (>>=) x f = bind f x
+let (>>|) x f = x >>= (fun x -> Some(f x))
+
+let text ppf s = Format.fprintf ppf "\"%s\"" (Escape.string s)
+
+
+let list = Format.pp_print_list
+
+let fp = Format.fprintf
+let premtext ?(pre="") ppf =
+  list ~pp_sep:(fun ppf () -> fp ppf "@,") (fun ppf -> fp ppf "%s%a" pre text)
+    ppf
+
+let mtext = premtext ~pre:""
+
+
+let pp_msg ?(pre="") ppf = function
+  | Singular {id;translation} ->
+    fp ppf "%smsgid %a@," pre mtext id;
+    if translation = [] then ()
+    else fp ppf "%smsgstr %a@," pre mtext translation;
+  | Plural { id; plural; translations } ->
+    fp ppf "%smsgid %a@," pre mtext id ;
+    fp ppf "%smsgid_plural %a@," pre mtext plural;
+    let pr (n,l) = match l with
+    | [] -> ()
+    | a :: q ->
+      fp ppf "%smsgstr[%d] %a@," pre n text a;
+      premtext ~pre ppf q in
+    List.iter pr translations
+
+
+let pp ppf entry =
+  let f x = Format.fprintf ppf x in
+  f "@[<v>";
+  List.iter (f "# %s@,") entry.comments.translator;
+  List.iter (f"#. %s@,") entry.comments.programmer;
+  f "#: %s:%d@," entry.location.file entry.location.line;
+  List.iter (f "#, %s@,") entry.flags;
+  List.iter (f "msgctxt %a@," text) entry.context;
+  entry.previous >> pp_msg ~pre:"#| " ppf;
+  pp_msg ppf entry.msg;
+  f "@]@."
+
+
+let make info loc comment src =
+  let msg = if info.plural = true then
+     Plural { id = src; plural = src; translations = [0,src] }
+    else
+       Singular { id = src; translation = src } in
+  { comments = { programmer = comment; translator = [] };
+    location =
+      (let s = loc.Location.loc_start in
+       Lexing.{ file = s.pos_fname; line = s.pos_lnum });
+    flags = [];
+    context = info.context;
+    previous = None;
+    msg
+  }
+
+type 'a return = {return: 'r. 'a -> 'r  } [@@unboxed]
+let with_return (type a) f =
+  let exception Return of a in
+  try f { return = (fun x -> raise (Return x)) } with
+  | Return x -> x
+
+let may_return f = with_return (fun r -> f r; None)
+
+let expr_payload filter f attrs =
+  let elt {return} attr =
+    if filter attr then
+      match snd attr with
+      | PStr [ { pstr_desc = Pstr_eval (e,_); _}  ] ->
+        return (f e)
+      | _ -> () in
+  may_return (fun return -> List.iter (elt return) attrs)
+
+let const f x = Some(f x)
+
+let attributes attrs =
+  expr_payload i18n_key (
+    function
+    | {pexp_desc= Pexp_ident {txt=L.Lident ("all"|"none" as status);_};_} ->
+      Some (status = "all")
+    | _ -> None ) attrs
+
+let is_printf f = match f.pexp_desc with
+  | Pexp_ident { txt=L.(Ldot(Lident "I18n", f)); _ } ->
+    begin match f with
+      | "printf" | "sprintf" -> Some 0
+      | "kfprintf" -> Some 2
+      | "fprintf" -> Some 1
+      | _ -> None
+    end
+  | _ -> None
+
+
+let exdoc  = function
+  | { Location.txt = "ocaml.doc"|"doc"; _ },
+    PStr [
+      { pstr_desc = Pstr_eval ({
+            pexp_desc = Pexp_constant Pconst_string (s,_); _ },_)
+      }
+    ] -> [s]
+  | _ -> []
+
+let exdocs = List.fold_left (fun l x -> exdoc x @ l) []
+
+let str info e = match e.pexp_desc with
+  | Pexp_constant Pconst_string (s,_) ->
+    Some (make info (e.pexp_loc) (exdocs e.pexp_attributes) [s]  )
+  | _ -> None
+
+let register ppf entry =
+  Format.fprintf ppf "%a" pp entry
+
+let apply ppf e =
+  match e.pexp_desc with
+  | Pexp_apply (f, l) ->
+    f
+    |> is_printf
+    >>= List.nth_opt l
+    >>| snd
+    >>= str info_default
+    >> register ppf
+  | _ -> ()
+
+
+let with_attrs attrs k =
+    match attributes attrs with
+    | Some x ->
+      let old = !status in
+      fun y -> (status := x; let r = k y in status := old; r )
+    | None -> k
+
+let super = Iter.default_iterator
+
+let default x y = match y with
+  | None -> x
+  | Some y -> y
+
+let expr ppf iter e =
+  with_attrs e.pexp_attributes (fun () ->
+      apply ppf e;
+      let info = default info_default
+        @@ expr_payload i18n_key (const metadata) e.pexp_attributes in
+      if (List.exists i18n_key e.pexp_attributes || !status) then
+        begin match str info e with
+          | Some entry -> register ppf entry
+          | None -> super.expr iter e
+        end
+      else
+        super.expr iter e
+    ) ()
+
+let structure_item iter i =
+  match i.pstr_desc with
+  | Pstr_eval (e,attrs) ->
+    with_attrs attrs (iter.Iter.expr iter) e
+  | Pstr_attribute a ->
+    maybe ((:=) status) (attributes [a])
+  | _ -> super.structure_item iter i
+
+let value_binding iter vb =
+  with_attrs vb.pvb_attributes (super.value_binding iter) vb
+
+let iter ppf =
+  let expr = expr ppf in
+  { super with
+    expr;
+    structure_item;
+    value_binding;
+  }
+
+
+let parse ppf file =
+  let iter = iter ppf in
+  Pparse.parse_implementation Format.err_formatter
+    ~tool_name:"ppxtract" file
+  |> iter.structure iter
+
+let rec explore prefix filter file =
+  let file =
+    String.concat "/" @@ List.rev_append prefix [file] in
+  if Sys.is_directory file then
+    List.concat @@ List.map (explore (file::prefix) filter)
+      (Array.to_list @@ Sys.readdir file)
+  else if filter file then [file] else []
+
+(*
+let rec explore ppf prefix filter file =
+  let file =
+    String.concat "/" @@ List.rev_append prefix [file] in
+  if Sys.is_directory file then
+    Array.iter (explore ppf (file::prefix) filter) (Sys.readdir file)
+  else if filter file then
+    parse ppf file
+*)
+
+let ml_file f = match Filename.extension f with
+  | ".ml" -> true
+  | ".mlt" -> true
+  | _ -> false
+
+
+let header =
+  Singular { id = [""]; translation =
+                          [ "Content-Type: text/plain; charset=UTF-8\n";
+                            "Content-Transfer-Encoding: 8bit\n";
+                            "Language: en_US\n";
+                            "Plural-Forms: nplurals=2; plural=(n != 1);\n";
+                            "Project-Id-Version: OCaml - babilim\n";
+                            "POT-Creation-Date: \n";
+                            "PO-Revision-Date: \n";
+                            "Last-Translator: \n";
+                            "Language-Team: \n";
+                            "MIME-Version: 1.0\n";
+                          ]
+           }
+
+let () =
+  let f = ref "ocaml.pot" in
+  let files = ref [] in
+  let register_files file = files := explore [] ml_file file :: !files in
+  let output = "-pot", Arg.Set_string f, "output pot file" in
+  Arg.parse [output] register_files "ppxtract files";
+  let ppf = Format.formatter_of_out_channel (open_out !f) in
+  Format.fprintf ppf "@[<v>%a@]@." (pp_msg ~pre:"") header;
+  List.iter (List.iter (parse ppf)) !files

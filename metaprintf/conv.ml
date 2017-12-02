d6df0071b8746b66fd12c34db8d3108416bf8307
octachron <octa@polychoron.fr>
2017-12-02 19:13:24 +0100

    Conversion from Format

---
+++ metaprintf/conv.ml
@@ -0,0 +1,89 @@
+module W = Witness
+
+
+open CamlinternalFormatBasics
+
+let padding (type a b c d) (y:(a,b) padding)
+  : (a , b ) W.h  =
+  match y with
+    | No_padding -> H []
+    | Lit_padding _ -> H []
+    | Arg_padding _ -> H [Int]
+
+let pr (type a b) (y:(a,b) precision)
+  : (a, b) W.h  =
+  match y with
+    | No_precision -> H []
+    | Lit_precision _ -> H []
+    | Arg_precision -> H [Int]
+
+exception Unsupported of string
+let unsupported s = raise (Unsupported s)
+open W
+let (@) = (@/)
+let rec typer : type a c d e f. (a,fmta,unit,d,e,f) fmt -> (a,f) W.h =
+  fun fmt -> match fmt with
+    | Char f -> Char <::> f
+    | Caml_char f -> Char <::> f
+    | String (p,f) -> (padding p) @ (String <::> f)
+    | Caml_string (p,f) -> (padding p) @ (String <::> f)
+    | Int (k,pa,pre,f) -> padding pa @ pr pre @ (Int <::> f)
+    | Int32 (k,pa,pre,f) -> padding pa @ pr pre @ (Int32 <::> f)
+    | Int64 (k,pa,pre,f) -> padding pa @ pr pre @ (Int64 <::> f)
+    | Nativeint (k,pa,pre,f) -> padding pa @ pr pre @ (Nativeint <::> f)
+    | Bool f -> Bool <::> f
+    | Float(_,pa,pre,f) -> padding pa @ pr pre @ (Float <::> f)
+    | Theta f -> Theta <::> f
+
+    | Flush f -> typer f
+    | String_literal (_,f) -> typer f
+    | Char_literal (_,f) -> typer f
+    | Formatting_lit(_,f) -> typer f
+
+    | Formatting_gen(f,f') -> boxes f @/ typer f'
+
+    | End_of_format -> H []
+    | Alpha f -> let W.H r = typer f in H (A :: r)
+
+    | Format_subst _ -> unsupported "Format substitution %(fmt%)"
+    | Format_arg _ -> unsupported "Format arg %{fmt%}"
+    | Reader _ -> unsupported "Reader r"
+    | Scan_char_set _ -> unsupported "Scanf char set […]"
+    | Scan_get_counter _ -> unsupported "Scanf get_counter %n/%l/%N/µL"
+    | Scan_next_char _ -> unsupported "Scanf next_char %0c"
+    | Ignored_param _ -> unsupported "Scanf %_…"
+    | Custom _ -> unsupported "Custom"
+
+and boxes:  type a c d e f. (a,fmta,unit,d,e,f) formatting_gen -> (a,f) W.h =
+  function
+  | Open_tag Format (f,_) -> typer f
+  | Open_box Format (f,_) -> typer f
+
+
+and (<::>): type x ll lm r d e f.
+  (x, ll * lm) s arg -> (r,fmta,unit,d,e,f) fmt -> (x->r,f) h =
+  fun x f -> let W.H r = typer f in H (refresh x :: r)
+
+type anyfmt = Any: _ format6 -> anyfmt [@@unboxed]
+
+module Map = struct
+  include Map.Make(struct type t = anyfmt let compare = compare end)
+
+  let add:
+    ('a,fmta,_,_,_,unit) format6
+    -> ('ab * 'a, unit * unit) l -> 'ab -> box t -> box t =
+    fun fmt spec f m ->
+      add (Any fmt) (Box(spec,f)) m
+
+  let find (Format (c,_) as fmt) m =
+    let box = find (Any fmt) m in
+    let expected_spec = typer c in
+    unbox expected_spec box
+
+end
+
+let m = Map.empty
+
+let m = Map.add "%d/%d" [Int;Int] (Format.printf "A nice ratio %d/%d") m
+
+let () = Map.find "%d/%d" m 4 5
--- metaprintf/witness.ml
+++ metaprintf/witness.ml
@@ -1,27 +1,49 @@
-type fmt = Format.formatter
+type fmta = Format.formatter
 
-type 'a cp = (fmt -> 'a -> unit)
-type show = Show: (fmt -> 'a -> unit) * 'a -> show
+type 'a cp = (fmta -> 'a -> unit)
+type show = Show: (fmta -> 'a -> unit) * 'a -> show
 
-type (_,_) arg =
-  | Int: ('l * 'm, (int->'l) * (int -> 'm) ) arg
-  | Float: ('l * 'm , (float->'l) * (float -> 'm)) arg
-  | A: ('l * 'm , (show -> 'l) * ('a cp -> 'a -> 'm) ) arg
-  | String: ('l * 'm, (string -> 'l) * (string -> 'm) ) arg
+type ('x,'y,'p) a = <x:'l*'m; fx:(('x -> 'l) * ('y -> 'm))>
+  constraint 'p = 'l * 'm
+type ('x,'p) s = <x:'l*'m; fx:(('x -> 'l) * ('x -> 'm))>
+  constraint 'p = 'l * 'm
+type ('x,'y, 'p) d = <x:'l*'m; fx:(('x -> 'l) * ('y1 -> 'y2 -> 'm))>
+  constraint 'p = 'l * 'm
+  constraint 'y = 'y1 -> 'y2
 
-type (_,_) l =
+type _ arg =
+  | Char:  (char, _ ) s arg
+  | Int: (int, _ ) s arg
+  | Int32: (int32, _ ) s arg
+  | Int64: (int64, _ ) s arg
+  | Nativeint: (nativeint, _ ) s arg
+  | Bool: (bool, _ ) s arg
+  | Float: (float, _ ) s arg
+  | String: (string, _) s arg
+  | Theta: ((fmta -> unit),_) s arg
+  | A: (show, 'a cp -> 'a, _ ) d arg
+(*  | Box: ('inside * 'more_inside, 'middle) l
+          -> <x:'middle; fx:'inside * 'more_inside> arg*)
+
+and (_,_) l =
   | []: ('tail * 'moretail ,'tail *'moretail) l
-  | (::): ('l , 'res) arg
-          * ('l,'tail) l -> ('res,'tail) l
+  | (::): <x:'l * 'm ; fx:'res * 'resm > arg * ('l * 'm, 'tail) l
+    -> ('res * 'resm,'tail) l
 
 
 type (_,_) eq = Eq: ('x,'x) eq
 type dyn = Dyn: ('a * 'b,'c * 'd) l -> dyn
 
-
+let rec (@): type ls l2 t. (ls, l2) l -> (l2,t) l -> (ls,t) l =
+  fun l r ->
+    match l with
+    | [] -> r
+    | a :: q -> a :: ( q @ r )
 
 let eq_arg: type x left left_more right right_more.
-  (x,left * left_more) arg -> (x,right * right_more) arg  -> (left,right) eq option =
+  < x:x; fx:(left * left_more) > arg ->
+  < x:x; fx:(right * right_more) > arg
+  -> (left,right) eq option =
   fun x y ->
   match x, y with
   | Int, Int -> Some Eq
@@ -31,8 +53,8 @@ let eq_arg: type x left left_more right right_more.
   | _ -> None
 
 let conv_arg: type x left right left_more right_more.
-  (x,left * left_more) arg -> (x,right * right_more) arg  ->
-  (x,left * left_more) arg  option =
+  <x:x; fx:left * left_more > arg -> <x:x; fx:right * right_more> arg  ->
+  <x:x; fx:left * left_more > arg  option =
   fun x y ->
   match x, y with
   | Int, Int -> Some Int
@@ -70,6 +92,7 @@ let rec (===): type left left_more right right_more t.
       | None -> None
       | Some Eq -> Some Eq
     end
+  (*  | Box il :: l, Box ir :: r -> (il @ l ) === (ir @ r )*)
   | _ -> None
 
 
@@ -101,19 +124,10 @@ let rec (<:): type left left_more right right_more t t2.
       | None -> None
       | Some r -> Some (String :: r )
     end
+  (*  | Box x :: l, Box y :: r -> (x @ l ) <: ( y @ r )*)
   | _ -> None
 
-           (*match eq_arg a b with
-      | None -> None
-      | Some Eq -> Some Eq
 
-         *)
-
-let rec (@): type ls l2 t. (ls, l2) l -> (l2,t) l -> (ls,t) l =
-  fun l r ->
-    match l with
-    | [] -> r
-    | a :: q -> a :: ( q @ r )
 
 let remember (type a b c d) (x:(a*b,c*d) l ) (Dyn y) =
   y <: x
@@ -121,32 +135,84 @@ let remember (type a b c d) (x:(a*b,c*d) l ) (Dyn y) =
 let (<::)(Dyn l) x = l <: x
 
 let l = [A;A;Int;Float]
-let test = [A;A;Int;Float] === [A;A;Int;Float]
-let retype = (Dyn l) <:: l
+(* let test = [A;A;Int;Float] === [A;A;Int;Float] (*weak*) *)
+(* let retype = (Dyn l) <:: l (* weak *) *)
 
 type box = Box: ('core * _, unit * _) l * 'core -> box
 
 
-let rec do_nothing: type a b c. (a * b, unit * unit ) l -> b = fun spec ->
+
+
+type (_,_) h = H: ('a * 'b,'c * 'd) l -> ('b,'d) h
+
+let rec refresh: type a b c d e. (a, b * c) s arg ->
+  (a, d * e) s arg
+  = function
+  | Int -> Int
+  | Float -> Float
+  | Char -> Char
+  | Int32 -> Int32
+  | Int64 -> Int64
+  | Bool -> Bool
+  | Nativeint -> Nativeint
+  | String -> String
+  | Theta -> Theta
+
+let rec (@/): type ls l2 t. (ls, l2) h -> (l2,t) h -> (ls,t) h =
+  fun (H x) (H y) -> match x with
+    | [] -> H y
+    | Char :: q -> single Char q (H y)
+    | Int :: q -> single Int q (H y)
+    | Int32 :: q -> single Int32 q (H y)
+    | Int64 :: q -> single Int64 q (H y)
+    | Nativeint :: q -> single Nativeint q (H y)
+    | Float :: q -> single Float q (H y)
+    | Bool :: q -> single Bool q (H y)
+    | String :: q -> single String q (H y)
+    | Theta :: q  -> single Theta q (H y)
+    | (A :: q) ->
+      let H r = ( H q @/ H y ) in
+      H ( A :: r )
+(*   | ( (Box b) :: q ) -> H (b @ q) @/ (H y) *)
+and single: type x ll lm r rf t.
+  (x, ll * lm) s arg -> (ll * lm, rf * r) l ->
+  (r,t) h -> (x->lm,t) h = fun x q y ->
+  let H r = H q @/ y in
+  H ( refresh x :: r)
+
+let rec do_nothing: type a b c. (a , unit) h -> a = fun (H spec) ->
   match spec with
   | [] -> ()
-  | Int :: q -> fun _ -> do_nothing q
-  | Float :: q -> fun _ -> do_nothing q
-  | String :: q -> fun _ -> do_nothing q
-  | A :: q -> fun _ _ -> do_nothing q
+  | Char :: q -> fun _ -> do_nothing (H q)
+  | Int :: q -> fun _ -> do_nothing (H q)
+  | Int32 :: q -> fun _ -> do_nothing (H q)
+  | Int64 :: q -> fun _ -> do_nothing (H q)
+  | Nativeint :: q -> fun _ -> do_nothing (H q)
+  | Bool :: q -> fun _ -> do_nothing (H q)
+  | Float :: q -> fun _ -> do_nothing (H q)
+  | String :: q -> fun _ -> do_nothing (H q)
+  | A :: q -> fun _ _ -> do_nothing (H q)
+  | Theta :: q -> fun _ -> do_nothing (H q)
+(* | Box l :: q -> do_nothing (H l @/ H q) *)
 
 
-let rec unbox: type a b c. (a * b, unit * unit) l -> box -> b =
-  fun spec (Box(spec',f)) ->
-  match spec, spec' with
-  | [], [] -> ()
-  | Int :: l, Int :: r ->
-    fun (n:int) -> unbox l (Box(r, f n))
-  | Float :: l, Float :: r ->
-    fun (x:float) -> unbox l (Box(r, f x))
-  | A :: l, A :: r ->
-    fun show x -> unbox l (Box(r, f (Show(show,x))))
-  | _ -> do_nothing spec
+
+
+let rec unbox: type a b c. (b, unit) h -> box -> b =
+  fun (H spec) (Box(spec',f)) ->
+    match spec, spec' with
+    | [], [] -> ()
+    | Char :: l, Char :: r ->
+      fun (n:char) -> unbox (H l) (Box(r, f n))
+    | Int :: l, Int :: r ->
+      fun (n:int) -> unbox (H l) (Box(r, f n))
+    | Float :: l, Float :: r ->
+      fun (x:float) -> unbox (H l) (Box(r, f x))
+    | A :: l, A :: r ->
+      fun show x -> unbox (H l) (Box(r, f (Show(show,x))))
+(*    | Box q :: l, r -> unbox (H q @/ H l) (Box(r,f))
+      | l , Box b ::r -> unbox (H l) (Box(b@r,f)) *)
+    | _ -> do_nothing (H spec)
 
 let box =
   let f i (Show(f,x)) =
@@ -154,4 +220,4 @@ let box =
     Format.printf "show:%a" f x in
   Box([Int;A],f)
 
-;; unbox [Int;A] box 6 Format.pp_print_string "Hi"
+;; unbox (H [Int;A]) box 6 Format.pp_print_string "Hi"

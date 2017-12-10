86007478e9a0b223e44b0b3ec0f2235184c8e332
octachron <octa@polychoron.fr>
2017-12-10 01:49:16 +0100

    Fusion babilim and metaprintf

--- metaprintf/babilim.ml
+++ metaprintf/babilim.ml
@@ -1,13 +1,13 @@
-let dir = Configuration.share ^ "/babilim"
-module T = Metaprintf.Tmap
+let dir = Configuration.share ^ "/babilim/"
+module T = Tmap
 
 let set_map f =
-  match T.Store.read @@ f ^ ".bo" with
+  match T.Store.read @@ dir ^ f ^ ".bo" with
   | None -> ()
   | Some m ->
     let map = m.T.Store.translations in
     let T.Implementation.{kfprintf; knfprintf } = T.Implementation.from_map map in
-      I18n.hook := I18n.{ kfprintf; knfprintf }
+      I18n.hook := { I18n.kfprintf; I18n.knfprintf }
 
 let () =
   Clflags.add_arguments __LOC__
